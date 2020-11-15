115d6ebe38be9b47b356a8e600eaf3d4130e974c
Bruce <ydjidil@163.com>
2020-11-16 00:50:35 +0800

    valid-anagram

--- Week_02/[242]有效的字母异位词.java
+++ Week_02/[242]有效的字母异位词.java
@@ -1,16 +1,30 @@
-public boolean isAnagram(String s, String t) {
-    if (s.length() != t.length()) {
-        return false;
-    }
-    int[] counter = new int[26];
-    for (int i = 0; i < s.length(); i++) {
-        counter[s.charAt(i) - 'a']++;
-        counter[t.charAt(i) - 'a']--;
-    }
-    for (int count : counter) {
-        if (count != 0) {
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
             return false;
         }
+        Map<Character, Integer> map = new HashMap<>();
+        char[] cs = s.toCharArray();
+        char[] ts = t.toCharArray();
+        for (int i = 0; i < cs.length; i ++) {
+            Integer scount = map.get(cs[i]);
+            if (scount != null) {
+                map.put(cs[i], ++ scount);
+            } else {
+                map.put(cs[i], 1);
+            }
+            Integer tcount = map.get(ts[i]);
+            if (tcount != null) {
+                map.put(ts[i], -- tcount);
+            } else {
+                map.put(ts[i], -1);
+            }
+        }
+        for (Integer count : map.values()) {
+            if (count != 0) {
+                return false;
+            }
+        }
+        return true;
     }
-    return true;
 }
