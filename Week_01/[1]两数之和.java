b960d0aa8bcb1853d32d5f589f2c6bd054ba41f7
Bruce <ydjidil@163.com>
2020-12-12 11:58:48 +0800

    Hash version

--- Week_01/[1]两数之和.java
+++ Week_01/[1]两数之和.java
@@ -1,11 +1,25 @@
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++ ) {
+    // 暴力版本
+    public int[] twoSum1(int[] nums, int target) {
+        for (int i = 0; i < nums.length - 1;i ++) {
             for (int j = i + 1; j < nums.length; j ++) {
-                if (nums[i] + nums[j] == target) {
+                if (nums[i] == target - nums[j]) {
                     return new int[] {i, j};
                 }
             }
+        }
+        return new int[] {-1, -1};
+    }
+
+    // Hash版本
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i ++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[] {map.get(target - nums[i]), i};
+            } 
+            map.put(nums[i], i);
+            
         }
         return new int[] {-1, -1};
     }
